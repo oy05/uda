b9557ff2bfdf3724ee9919590041aaca1ce2c156
oy05 <89623558+oy05@users.noreply.github.com>
2022-05-29 15:21:20 -0700

    Update download.sh

--- back_translate/download.sh
+++ back_translate/download.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 #!/bin/bash
 cd learn2aug/emnlp2022/stance-detection/backtranslation
-cp tweets.txt ~/uda/backtranslate
+cp tweets.txt ~/uda/back_translate
 filename=tweets.txt
 cd
 
